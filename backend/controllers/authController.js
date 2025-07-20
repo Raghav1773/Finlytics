06cc66a08c9252846104fb6829279c594d9ba3af
Raghav1773 <raghavsharmaa1773@gmail.com>
2025-07-20 16:42:46 +0530

    added the whole working project

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 
 // generatee JWT token
 const generateToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "100h" });
 };
 
 //register user
---
+++ frontend/finlytics/src/components/Dashboard/RecentIncomeWithChart.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react'
+import CustomPieChart from '../Charts/CustomPieChart'
+
+const COLORS = ["#875CF5", "#FA2C37", "#FF6900", "#4f39f6", "#00C9A7", "#FFB400", "#FF6F61", "#6A0572"];
+
+const RecentIncomeWithChart = ( {data, totalIncome }) => {
+
+        const [chartData, setChartData] = useState([]);
+
+    const prepareChartData = () => {
+        const dataArr = data?.map((item) => ({
+            name: item?.source,
+            amount: item?.amount,
+        }));
+
+        setChartData(dataArr);
+    };
+
+    useEffect(() => {
+        prepareChartData();
+
+        return () => {};
+    }, [data]);
+
+  return (
+    <div className="card">
+        <div className="flex items-center justify-between">
+            <h5 className="text-lg">Income Overview</h5>
+        </div>
+
+        <CustomPieChart 
+          data={chartData}
+          label="Total Income"
+          totalAmount={`$${totalIncome}`}
+          showTextAnchor
+          colors={COLORS}
+        />
+    </div>
+)
+}
+
+export default RecentIncomeWithChart
