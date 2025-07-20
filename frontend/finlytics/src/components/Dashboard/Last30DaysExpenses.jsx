2fd64c5df31029fc228b49cf994ff75dc4c6dc91
Raghav1773 <raghavsharmaa1773@gmail.com>
2025-07-20 16:59:02 +0530

    fixed a UI bug

--- frontend/finlytics/src/components/Dashboard/Last30DaysExpenses.jsx
+++ frontend/finlytics/src/components/Dashboard/Last30DaysExpenses.jsx
@@ -16,7 +16,7 @@ const Last30DaysExpenses = ({data}) => {
 
 
   return (
-    <div className="card-col-span-1">
+    <div className="card col-span-1">
         <div className="flex items-center justify-between">
             <h5 className="text-lg">Last 30 Days Expenses</h5>
         </div>
